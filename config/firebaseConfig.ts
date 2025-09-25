807ab089c6b4bae61dcb8cac3c8a0e8387a58c27
Nikol <alicia.felisha@binus.ac.id>
2025-09-25 11:30:36 +0700

    feat: setup firebase

---
+++ config/firebaseConfig.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+let app: FirebaseApp;
+if (getApps().length === 0) {
+  app = initializeApp(firebaseConfig);
+} else {
+  app = getApp();
+}
+
+// Buat nanti Auth, DB
+// const auth = getAuth(app);
+// const firestore = getFirestore(app);
+// const analytics = getAnalytics(app);
+// export { auth, firestore };
+// export const db = firestore;
