cb9e49bfffd56c1363db6817f54d3a4e07318849
Nikol <alicia.felisha@binus.ac.id>
2025-10-05 15:39:24 +0700

    feat: inject

---
+++ constants/utils/colors.ts
@@ -0,0 +1,28 @@
+export const COLORS = {
+  // Primary
+  primary: "#E39400",
+  primary2nd: "#FCBC03",
+  primary3rd: "#FFD661",
+  primary4th: "#F5F2E7",
+
+  // Tersier
+  tersier: "#4C13A2",
+  tersier2nd: "#01C1D6",
+
+  // Neutral
+  neutral50: "#F8FAFC",
+  neutral100: "#F1F5F9",
+  neutral300: "#CBD5E1",
+  neutral500: "#64748B",
+  neutral700: "#334155",
+  neutral900: "0F172A",
+
+  //Alert Color
+  success: "#10B981",
+  warning: "#F59E0B",
+  error: "EF4444",
+
+  // Other Neutrals
+  white: "#FFFFFF",
+  black: "#181818",
+};
